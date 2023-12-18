140afe5e97ea388c074baa2e02516971edab25bc
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2023-12-19 02:27:27 +0500

    fully responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='max-w-[72rem] mx-auto py-10'>
+    <div className='max-w-[72rem] mx-auto md:py-10 py-2'>
       <div className='flex flex-col-reverse items-center md:flex-row md:justify-between'>
-        <div className='text-center md:text-left mb-4 md:mb-0'>
+        <div className='text-center md:text-left mt-4 md:mt-0'>
           <p className='text-[1rem]'>© Copyright 2023, All Rights Reserved</p>
         </div>
         <div className='w-full md:w-auto text-center md:text-left text-[1rem] font-normal text-[#12141D]'>
